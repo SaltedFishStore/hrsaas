9647239fa8428a1ba4ea22f750155090a98f30b0
SaltedFishStore <liu982018@163.com>
2021-03-30 00:25:17 +0800

    封装Vuex的登录Action并处理token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,34 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+
+const state = {
+  token: getToken() // token初始状态
+}
+
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    setToken(token) // 设置缓存
+  },
+
+  removeToken(state) {
+    state.token = null
+    removeToken() // 清除token
+  }
+}
+
+const actions = {
+  async login(context, data) {
+    const response = await login(data)
+    if (response.data.success) {
+      context.commit('setToken', response.data.data)
+    }
+  }
+}
+
 export default {
-  nameSpaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  namespaced: true,
+  state,
+  mutations,
+  actions
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,18 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = 'token'
 
+// 读取缓存中的token
 export function getToken() {
   return Cookies.get(TokenKey)
 }
 
+// 将token写入缓存
 export function setToken(token) {
   return Cookies.set(TokenKey, token)
 }
 
+// 删除缓存中的token
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
