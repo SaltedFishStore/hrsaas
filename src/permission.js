a8a87c8e707da3d1a227012a60e5771b90bcc512
SaltedFishStore <liu982018@163.com>
2021-03-30 01:09:11 +0800

    根据token处理主页的访问权限问题

---
+++ src/permission.js
@@ -0,0 +1,31 @@
+import router from '@/router'
+import store from '@/store'
+import NProgress  from 'nprogress'
+import 'nprogress/nprogress.css'
+
+const whiteList = ['/login', '/404'] // 白名单
+
+// 路由前置守卫
+router.beforeEach((to, from, next) => {
+  NProgress.start() // 开启进度条
+  // 判断是否有token
+  if (store.getters.token) {
+    if (to.path === 'login') { // 有token跳转登录页
+      next('/') // 跳转到首页
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.indexOf(to.path) > -1) {
+      next() // 在白名单中，直接放行
+    } else {
+      next('/login') // 跳到登录页
+    }
+  }
+  NProgress.done() // 关闭进度条
+})
+
+// 路由后置守卫
+router.afterEach(() => {
+  NProgress.done() // 关闭进度条
+})
