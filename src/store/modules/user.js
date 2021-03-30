0574b7ae9e54bc284a11836310bdc02be6b569ca
SaltedFishStore <liu982018@163.com>
2021-03-30 21:30:02 +0800

    封装获取用户资料的action并共享用户状态

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,9 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 
 const state = {
-  token: getToken() // token初始状态
+  token: getToken(), // token初始状态
+  userInfo: {}
 }
 
 const mutations = {
@@ -10,17 +11,31 @@ const mutations = {
     state.token = token
     setToken(token) // 设置缓存
   },
-
   removeToken(state) {
     state.token = null
     removeToken() // 清除token
+  },
+  // 设置用户信息
+  setUserInfo(state, userInfo) {
+    state.userInfo = { ...userInfo } // 使用浅拷贝的方式赋值对象
+  },
+  // 删除用户信息
+  removeUserInfo(state) {
+    state.userInfo = {}
   }
 }
 
 const actions = {
+  // 登录
   async login(context, data) {
     const result = await login(data)
     context.commit('setToken', result)
+  },
+  // 获取用户信息
+  async getUserInfo(context) {
+    const result = await getUserInfo()
+    context.commit('setUserInfo', result)
+    return result
   }
 }
 
