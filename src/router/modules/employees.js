b88038b83cbff6c5a6ce294b45e085b03008bf5a
SaltedFishStore <liu982018@163.com>
2021-03-30 23:49:18 +0800

    完成其他模块的页面和路由的快速搭建

---
+++ src/router/modules/employees.js
@@ -0,0 +1,18 @@
+import Layout from '@/layout'
+
+export default {
+  path: '/employees',
+  name: 'employees',
+  component: Layout,
+  // 配置二级路由表
+  children: [
+    {
+      path: '', // 什么都不写，表示改路由为当前二级路由的默认路由
+      component: () => import('@/views/employees'),
+      // 路由元信息
+      meta: {
+        title: '员工管理'
+      }
+    }
+  ]
+}
