1cf4063cdb99d7a5bc548f79c3251e12c6cfe736
SaltedFishStore <liu982018@163.com>
2021-03-31 21:53:28 +0800

    将树形的操作内容单独抽提成组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -2,52 +2,10 @@
   <div class="dashboard-container">
     <div class="app-container">
       <el-card class="tree-card">
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px;">
-          <el-col>
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <!-- 下拉菜单 -->
-              <el-col>
-                <el-dropdown size="small">
-                  <span>
-                    操作<i class="el-icon-arrow-down" />
-                  </span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <tree-tools :tree-node="company" is-root />
 
         <el-tree :data="departs" :props="defaultProps" default-expand-all>
-          <el-row slot-scope="{ data}" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
-            <el-col>
-              <span>{{ data.name }}</span>
-            </el-col>
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <!-- 下拉菜单 -->
-                <el-col>
-                  <el-dropdown size="small">
-                    <span>
-                      操作<i class="el-icon-arrow-down" />
-                    </span>
-                    <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-            </el-col>
-          </el-row>
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
         </el-tree>
       </el-card>
     </div>
@@ -55,7 +13,12 @@
 </template>
 
 <script>
+import TreeTools from './components/tree-tools'
+
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
       departs: [
@@ -65,6 +28,10 @@ export default {
       ],
       defaultProps: {
         label: 'name'
+      },
+      company: {
+        name: '江苏传智播客教育科技股份有限公司',
+        manager: '负责人'
       }
     }
   }
